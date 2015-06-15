da5cc9f37f7473931702fb534c2b06f0355c38da
miekehendriks <m.hendriks-ab@student.fontys.nl>
2015-06-15 15:02:08 +0200

    Final styling
    type/audio switch button
    play audio button
    send audio/text next to type/audio button + semi transparant when not
    active

--- yammer3/www/js/controllers/audioController.js
+++ yammer3/www/js/controllers/audioController.js
@@ -5,20 +5,20 @@ app.controller('audioController', function($scope){
     var ticker;
     // console.log(src);
 
-    // var mediaRec = new Media(src,
-    //     // success callback
-    //     function() {
-    //         console.log("recordAudio():Audio Success");
-    //     },
+    var mediaRec = new Media(src,
+        // success callback
+        function() {
+            console.log("recordAudio():Audio Success");
+        },
 
-    //     // error callback
-    //     function(err) {
-    //         console.log("recordAudio():Audio Error: "+ err.message);
-    //     },
+        // error callback
+        function(err) {
+            console.log("recordAudio():Audio Error: "+ err.message);
+        },
 
-    //     function(status){
-    //         console.log('status changed: ', status);
-    //     });
+        function(status){
+            console.log('status changed: ', status);
+        });
 
     $scope.recordAudio = function(){
         console.log("RECOOOOORRD");
@@ -48,7 +48,8 @@ $scope.stopRecord = function(){
         document.getElementById("buttonTalk").style.boxShadow = "inset 0 0 0px 0px rgba(0, 0, 0, 0.2)";
 
     mediaRec.stopRecord();
-    document.getElementById("btUpload").style.display = "block";
+    document.getElementById("btUpload").style.opacity= "1";
+    document.getElementById("btUpload").disabled = false;
     document.getElementById("buttonPlay").style.display = "block";
 
         //document.getElementById("buttonWhite").style.backgroundColor = "#FCFCFC";
@@ -79,20 +80,40 @@ $scope.stopRecord = function(){
     $scope.switchType = function(){
         document.getElementById("buttonWhite").style.display = "none";
         document.getElementById("InputText").style.display = "block";
-        document.getElementById("btTalk").style.display = "none";
-        document.getElementById("btType").style.display = "block";
+        //document.getElementById("switchTypeTalk").style.display = "none";
+
+
+        document.getElementById("wrapperText").style.display = "none";
+        document.getElementById("wrapperAudio").style.display = "block";
+
+        /*document.getElementById("btType").style.display = "block";
+        document.getElementById("btPlay").style.display = "none";
+
+        document.getElementById("btUpload").style.display = "none";
+        document.getElementById("btUploadText").style.display = "block";*/
+
+
 
         var inputBox = document.getElementById("InputType");
         inputBox.onkeyup = function(){
-            document.getElementById("btUploadText").style.display = "block";
+            document.getElementById("btUploadText").style.opacity= "1";
+            document.getElementById("btUploadText").disabled = false;
         }
     }
 
     $scope.switchTypeBack = function(){
         document.getElementById("buttonWhite").style.display = "block";
         document.getElementById("InputText").style.display = "none";
-        document.getElementById("btTalk").style.display = "block";
-        document.getElementById("btType").style.display = "none";
+        //document.getElementById("switchTypeTalk").style.display = "block";
+
+        document.getElementById("wrapperText").style.display = "block";
+        document.getElementById("wrapperAudio").style.display = "none";
+
+       /* document.getElementById("btType").style.display = "none";
+        document.getElementById("btPlay").style.display = "block";
+
+        document.getElementById("btUpload").style.display = "block";
+        document.getElementById("btUploadText").style.display = "none";*/
     }
 
 
